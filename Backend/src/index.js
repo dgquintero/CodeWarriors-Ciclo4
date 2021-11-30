82d0e78405f5d6d2153b25ad14ab1a3331c36dde
Maria Sandoval <marysandovalrodriguez@hotmail.com>
2021-11-29 19:57:28 -0500

    Se reacomoda estructura del código del backend y se lanza a development

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,24 +1,6 @@
-import express from "express";
-// import graphqlHTTP from "express-graphql";
-const { graphqlHTTP } = require('express-graphql');
-import UserSchema from "./schemas/UserSchema"
-import { connect } from "./database";
+import { server } from './server';
+import './database';
 
-const app = express();
-connect();
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello world'
-    })
-});
-
-app.use('/graphql', graphqlHTTP({
-    graphiql:true,
-    schema: UserSchema,
-    context: {
-        messageId: 'test'
-    }
-}));
-
-app.listen(3000, () => console.log('Server on port 3000'))
+server.start({port: 3100}, ({port}) => {
+    console.log('Server on port', port);
+})
--- Backend2/src/index.js
+++
@@ -1,6 +0,0 @@
-import { server } from './server';
-import './database';
-
-server.start({port: 3100}, ({port}) => {
-    console.log('Server on port', port);
-})
